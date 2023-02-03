56f0ebc71c558702f48f8c71127a77438f8bc8f7
JAIN <janepark423@gmail.com>
2023-02-03 20:18:15 +0900

    viewpager

--- app/src/main/java/com/example/app_comins/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/app_comins/ViewPagerAdapter.kt
@@ -1,9 +1,18 @@
 package com.example.app_comins
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.LayoutInflater
 
 class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
+
+    private var layoutInflater : LayoutInflater? =null
+
+    val Image = arrayOf()
+
+
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         TODO("Not yet implemented")
     }
