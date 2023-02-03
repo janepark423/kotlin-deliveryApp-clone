b6693d934e0a1300a8393ab655d382797dd4c411
JAIN <janepark423@gmail.com>
2023-02-03 20:54:23 +0900

    viewpager

--- app/src/main/java/com/example/app_comins/MainActivity.kt
+++ app/src/main/java/com/example/app_comins/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.app_comins
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var viewpager : ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewpager = findViewById(R.id.viewpager) as ViewPager
+
+        val adapter = ViewPagerAdapter(this)
+        viewpager.adapter = adapter
+
     }
 }
