a3eab9a463cfe71ed91d3b8ba7fcb9c558e97fa0
JAIN <janepark423@gmail.com>
2023-02-03 19:06:30 +0900

    viewpager

--- app/src/main/java/com/example/app_comins/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/app_comins/ViewPagerAdapter.kt
@@ -1,3 +1,14 @@
 package com.example.app_comins
 
-class ViewPagerAdapter(private  val context : Context)
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
